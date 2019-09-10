46f3e77c104bddefd8fe1661e2f42a8fab64f3a2
hamza <hyousaf@ivytech.edu>
2019-09-10 09:50:08 -0400

    Initial commit

---
+++ src/chap1_15.java
@@ -0,0 +1,17 @@
+// Hamza Yousaf
+// 9/10/2019
+// Program display a math equation
+// Chapter 1, Exercise 1.5
+
+public class chap1_15 {
+
+        public static void main(String[] args) {
+
+            System.out.println("The equation = [(9.5 x 4.5) - (2.5 x 3)] / (45.5 - 3.5)");
+            System.out.println("Answer = ");
+            System.out.println((9.5 * 4.5) - (2.5 * 3) / (45.5 - 3.5));
+
+        }
+
+
+}
